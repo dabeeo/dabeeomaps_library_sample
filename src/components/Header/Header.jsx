55daff98327099bca0d6cc0ea9169928cdb2182d
jihun.kim <jihun.kim@dabee.com>
2022-07-25 11:58:31 +0900

    samplePage example page 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import styles from './Header.module.scss';
 import LOGOIMG from '../../imgs/logo.png';
+import { useNavigate } from 'react-router';
 
 const Header = ({ setLang }) => {
+
+    const nav = useNavigate();
+
+    function onLink() {
+        nav('/example');
+    }
+
     return(
         <div className={styles.header}>
             <img src={LOGOIMG} className={styles.logo}></img>
             <div className={styles.documents}>
                 {/* <a className={styles.link} href='https://developers.dabeeomaps.com/jsdocs/index.html' target='_blank'>API Reference</a> */}
                 {/* <a className={styles.link} href='https://developers.dabeeomaps.com/example/#index' target='_blank'>Examples</a> */}
+                <span className={styles.link} onClick={onLink}>Example</span>
                 <a className={styles.link} href='http://api-doc.dabeeomaps.com/' target="_blank">Documents</a>
                 <span className={styles.btn} onClick={() => setLang('en')}>ENG</span>
                 <span className={styles.btn} onClick={() => setLang('ko')}>한글</span>
