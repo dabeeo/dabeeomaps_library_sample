cf6ff1fa29bbc118659b5bd57c24f291e361b676
jihun.kim <jihun.kim@dabeeo.com>
2022-08-12 18:46:34 +0900

    event naming change

--- src/components/pages/Events/Events.jsx
+++ src/components/pages/Events/Events.jsx
@@ -21,15 +21,15 @@ const Events = () => {
             <div className={styles.miniTitle}>floor-changed</div>
             <div className={styles.texts}>층이 변경된 경우 변경된 층의 ID값을 반환합니다.</div>
             <code className={styles.code}>{`mapContainer.addEventListener("floor-changed", (e) => console.log(e.detail));`}</code>
-            <div className={styles.miniTitle}>will-floor-change</div>
+            <div className={styles.miniTitle}>floor-changing</div>
             <div className={styles.texts}>층이 변경되기 전 현재 층의 정보와 다음 층의 정보를 반환합니다.</div>
-            <code className={styles.code}>{`mapContainer.addEventListener("will-floor-change", (e) => console.log(e.detail));`}</code>
+            <code className={styles.code}>{`mapContainer.addEventListener("floor-changing", (e) => console.log(e.detail));`}</code>
             <div className={styles.miniTitle}>navi-complete</div>
             <div className={styles.texts}>navigation의 동작이 완료됬음을 감지합니다.</div>
             <code className={styles.code}>{`mapContainer.addEventListener("navi-complete", (e) => console.log(e.detail));`}</code>
-            <div className={styles.miniTitle}>redrawed-map</div>
+            <div className={styles.miniTitle}>render-complete</div>
             <div className={styles.texts}>층이 다시 그려졌을 때 실행됩니다. 다시 그려진 층의 정보를 반환합니다.</div>
-            <code className={styles.code}>{`mapContainer.addEventListener("redrawed-map", (e) => console.log(e.detail));`}</code>
+            <code className={styles.code}>{`mapContainer.addEventListener("render-complete", (e) => console.log(e.detail));`}</code>
             <div className={styles.miniTitle}>zoom-possible</div>
             <div className={styles.texts}>zoomIn, zoomOut의 상태값을 받아올 수 있습니다.</div>
             <code className={styles.code}>{`mapContainer.addEventListener("zoom-possible", (e) => console.log(e.detail));`}</code>
