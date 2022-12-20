531b9af39f279cff46035e3a7107af492d17bc38
Clara(신상림) <sangrim.shin@dabeeo.com>
2022-12-20 19:01:44 +0900

    04.08.01

--- src/components/pages/ReleaseNote/releaseNoteData.js
+++ src/components/pages/ReleaseNote/releaseNoteData.js
@@ -1,6 +1,14 @@
 export const releaseNoteData = [
     {
         title: '04.07.01',
-        content: ['geojson(상대좌표)데이터 overlay로 지도 위에 올리는 기능 추가', '복수개의 3d model(obj, gltf)을 overlay로 지도 위에  올리는 기능 추가'],
+        content: [
+            '2022.12.12',
+            'geojson(상대좌표)데이터 overlay로 지도 위에 올리는 기능 추가',
+            '복수개의 3d model(obj, gltf)을 overlay로 지도 위에  올리는 기능 추가',
+        ],
+    },
+    {
+        title: '04.08.01',
+        content: ['2022.12.20', 'model-load-complete 커스텀 이벤트 추가'],
     },
 ];
