6e7bf535eb1a782219bb80de675388469e6b6b3c
EmiliaBaek <minjy.baek@dabeeo.com>
2024-05-02 14:52:37 +0900

    cesium example 주소 변경

--- sample/cesiumWindAnamDong/main.js
+++
@@ -1,9 +0,0 @@
-import { Panel } from "./gui.js";
-import { Wind3D } from "./wind3D.js";
-var demo = false;
-const mode = {
-  debug: demo ? false : true,
-};
-
-const panel = new Panel();
-const wind3D = new Wind3D(panel, mode);
