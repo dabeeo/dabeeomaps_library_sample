876bcaf569407fcaa4e60860b06ec52e399311ea
Judy(백주영) <141113236+juyeong-baek@users.noreply.github.com>
2024-06-18 12:03:35 +0900

    fix: 통합지도 sample 코드 및 설명 수정

--- src/components/pages/multiMap/multiMapText.jsx
+++ src/components/pages/multiMap/multiMapText.jsx
@@ -3,96 +3,131 @@ import styles from '../GetText/GetText.module.scss';
 const MultiMapText = (
     <div className={styles.getStartedText}>
         <div className={styles.title}>통합지도</div>
-        <div className={styles.miniTitle}>하나의 지도에서 georeferencing 을 통해 실외지도 위에 실내지도를 표출할 수 있습니다.</div>
-        <div className={styles.texts}>georeferencing 한 지도는 항상 실외지도가 표출된 상태여야 합니다.</div>
-        <div className={styles.texts}>georeferencing 하지 않은 지도는 실외지도와 실내지도를 동시에 표출할 수 없습니다.</div>
-        <div className={styles.texts}>outdoor의 경우 indoor와 같은 레벨에 존재하는 building으로 생각하시면 좋습니다.</div>
-        <div className={styles.texts}>
-            따라서 indoor를 표출 중에 지도를 outdoor로 바꾸기 위해서는 changeFloor(outdoor_floor_id)가 아닌 addBuilding(outdoor_id)를 해야 합니다.
+        <div className={styles.miniTitle}>통합지도의 정의</div>
+        <div className={styles.textGroup}>
+            <div className={styles.texts}>studio4 에서 georeferencing 된 지도는 실외지도 위에 실내지도를 표출할 수 있습니다.</div>
+            <div className={styles.texts}>
+                이를 위해 building 개념이 추가되었으며 지도는 여러개의 building으로 구성되며 하나의 building은 여러 층으로 구성되어 있습니다.
+            </div>
+            <div className={styles.texts}>실외지도 또한 하나의 building의 개념이며 실외지도의 buildingId 와 floorId는 mapId 입니다.</div>
+            <div className={styles.texts}>georeferencing 한 지도에서는 실외지도가 기준이 됩니다. 따라서 실외지도위에 실내지도가 올라갑니다.</div>
+            <div className={styles.texts}>
+                mapOption의 enableGeoreferencing 값이 false 인경우, georeferencing 하지 않은 지도와 동일하게 동작합니다. (항상 하나의 층만 표출)
+            </div>
+            <pre>
+                <code className={styles.code}>
+                    {` 
+const mapOption = {
+    enableGeoreferencing: false // default 값: true
+}
+`}
+                </code>
+            </pre>
         </div>
 
-        <pre>
-            <code className={styles.code}>
-                {`
-mapOption = {
-	enableGeoreferencing: false // georeferencing 하지 않은 지도와 동일하게 동작합니다.
-}
-                `}
-            </code>
-        </pre>
-        <p>
-            georeferencing 한 통합지도를 사용하기 위해서는 아래의 순서대로 이용합니다.
-            <br />
-            1. 실외지도를 표출합니다.
-            <br />
-            1.1. mapOption에 floor 로 빌딩의 층을 입력하면 지도가 그려지지 않습니다.
-            <br />
-            1.2. floor 를 입력하지 하지 않으면 실외지도가 그려집니다.
-            <br />
-            2. 실외지도 위에 빌딩을 추가합니다.
-            <br />
-            3. 표출된 빌딩은 다른 층으로 층변경할 수 있습니다.
-            <br />
-            3.1. 그려져있지 않은 빌딩에서 층변경할 수 없습니다.
-            <br />
-            4. 실외지도 위에 그려진 빌딩을 제거합니다.
-        </p>
-        <pre>
-            <code className={styles.code}>{`
-// 실외지도가 있는 지도에서 floor 를 입력하지 않으면 기본층은 실외지도로 가장 먼저 실외지도가 그려지게 됩니다.
-const map = await dabeeoMaps.showMap(mapContainer, mapOption, mapData); // 1. 실외지도 표출
-await map.context.addBuilding(buildingId, floorId); // 2. 빌딩 추가
-await map.context.changeFloor(floorId); // 3. 빌딩 층 변경
-map.context.removeBuilding(buildingId); // 4. 그렸던 빌딩 제거
-            `}</code>
-        </pre>
-        <p>
-            georeferencing 하지 않은 통합지도를 사용하기 위해서는 아래의 순서대로 이용합니다.
-            <br />
-            1. 실외지도 혹은 원하는 빌딩의 층을 표출합니다.
-            <br />
-            1.1. mapOption에 floor 로 원하는 빌딩의 원하는 층 id 를 넣으면 해당 층이 가장 먼저 그려집니다.
-            <br />
-            1.2. floor 를 입력하지 않으면 실외지도가 가장 먼저 그려지게 됩니다.
-            <br />
-            2. 빌딩 혹은 실외지도를 switching하기 위해 addBuilding <b>혹은</b> changeFloor 을 통해 원하는 빌딩, 층으로 이동할 수 있습니다.
-            <br />
-            2.1. addBuilding(buildingId, floorId) 으로 이동하는 경우는 특정 빌딩의 특정 층으로 이동할 수 있습니다.
-            <br />
-            2.2. addBuilding() 을 사용하지 않고 changeFloor() 를 통해 다른 빌딩의 층으로도 이동할 수 있습니다.
-            <br />
-            2.3. 빌딩 혹은 실외지도로 이동하게 되면 기존에 그려져있던 빌딩 혹은 실외지도는 지워집니다.
-        </p>
-        <pre>
-            <code className={styles.code}>{`
+        <div>
+            <div className={styles.miniTitle}>georeferencing 된 지도</div>
+            <div className={styles.textGroup}>
+                <div className={styles.texts}>georeferencing 된 통합지도를 showMap 할 경우 outdoor 가 표출되며 층 지정이 불가능합니다.</div>
+
+                <div className={styles.texts}>georeferencing 된 통합지도를 사용하기 위해서는 아래의 순서대로 이용합니다.</div>
+                <div className={styles.texts}> 1. showMap을 통해 실외지도를 표출합니다.</div>
+                <pre>
+                    <code className={styles.code}>
+                        {` 
+const map = await dabeeoMaps.showMap(mapContaienr, mapOption, mapData)
+`}
+                    </code>
+                </pre>
+                <div className={styles.texts}> 2. changeFloor 혹은 addBuilding을 통해 실내지도를 추가, 층을 변경할 수 있습니다. (여러개 동시 가능)</div>
+                <pre>
+                    <code className={styles.code}>
+                        {` 
+await map.context.addBuilding(buildingId, floorId) 
+await map.context.changeFloor(floorId)
+`}
+                    </code>
+                </pre>
+                <div className={styles.texts}> 3. removeBuilding을 통해 실외지도 위에 그려진 실내지도를 제거합니다.</div>
+                <pre>
+                    <code className={styles.code}>
+                        {` 
+map.context.removeBuilding(buildingId); // 실외지도 위에 그려져있는 특정 빌딩 제거
+`}
+                    </code>
+                </pre>
+            </div>
+        </div>
+        <div>
+            <div className={styles.miniTitle}>enableGeoreferencing 옵션</div>
+            <div className={styles.textGroup}>
+                <div className={styles.texts}>georeferencing된 통합지도를 한번에 한 층씩 보여주고자 할 때 옵션을 false 로 지정합니다.</div>
+            </div>
+            <pre>
+                <code className={styles.code}>
+                    {` 
 const mapOption = {
+	enableGeoreferencing: false,
+} 
+`}
+                </code>
+            </pre>
+            <div className={styles.textGroup}>
+                <div className={styles.texts}>한번에 하나의 층만 표시할 수 있습니다.</div>
+                <div className={styles.texts}>아래의 순서대로 사용합니다.</div>
+                <div className={styles.texts}> 1. showMap을 통해 실외지도 혹은 원하는 층을 표출합니다.</div>
+                <div className={styles.details}> 1.1. 실외지도가 있는 통합지도의 경우 defaultFloorId 는 실외지도의 floorId 입니다.</div>
+                <pre>
+                    <code className={styles.code}>
+                        {` 
+const mapOption ={
 	floor: building1_floor1_id,
+	enableGeoreferencing: false,
 }
-// 실외지도가 있는 지도에서 floor 를 입력하지 않으면 기본층은 실외지도로 가장 먼저 실외지도가 그려지게 됩니다.
-const map = await dabeeoMaps.showMap(mapContainer, mapOption, mapData); // 1. 1번 빌딩 1층 표출
-await map.context.addBuilding(buildingId, floorId); // 2.1. 다른 빌딩의 다른 층 표출 (1번 빌딩 제거)
-await map.context.changeFloor(floorId) // 2.2. 다른 층으로 이동 (다른 빌딩의 층으로도 가능)
-            `}</code>
-        </pre>
-        <p>
-            georeferencing 된 통합지도에서 실외지도에서 실내지도를 표출할 때에는 building mask object 를 hide 해야 합니다.
-            <br />
-            그려진 빌딩과 building mask object 가 겹쳐져 빌딩이 보이지 않기 때문에 building mask object 를 hide 시켜야 합니다.
-            <br />
-            building mask object 의 경우 오브젝트를 클릭했을 때 refBuildingId 값을 반환합니다. 이 값은 해당 빌딩의 id 입니다.
-            <br />
-            아래의 함수들을 통해 building mask object 를 관리할 수 있습니다.
-            <br />
-            자세한 것은 예제를 통해 확인할 수 있습니다.
-        </p>
-        <pre>
-            <code className={styles.code}>{`
+
+const map = await dabeeoMaps.showMap(mapContainer, mapOption, mapData); // 빌딩 1번의 1층 표출
+`}
+                    </code>
+                </pre>
+                <div className={styles.texts}> 2. 실외지도 혹은 실내지도(building) 간 switching을 위해서는 addBuilding 혹은 changeFloor 를 사용합니다.</div>
+                <div className={styles.details}>
+                    2.1. addBuilding(buildingId, floorId) 와 changeFloor(floorId) 를 사용해 특정 빌딩의 특정 층으로 이동할 수 있습니다.
+                </div>
+                <div className={styles.details}>2.2. changeFloor() 를 사용할 때 floorId 만 입력하여 사용할 수 있습니다.</div>
+                <pre>
+                    <code className={styles.code}>
+                        {` 
+await map.context.addBuilding(building2, building2_floor2_id); // building2 의 2번 층 표출
+await map.context.changeFloor(building1_floor3_id); // 1번 빌딩의 floor3 표출
+`}
+                    </code>
+                </pre>
+            </div>
+        </div>
+        <div>
+            <div className={styles.miniTitle}>georeferencing 된 지도를 사용할 때 buildingMask 제어 방법</div>
+            <div className={styles.textGroup}>
+                <div className={styles.texts}>
+                    georeferencing 된 통합지도에서 실외지도 위에 실내지도를 표출할 때 겹치는 object를 buildingMask 로 지정해 관리할 수 있습니다.
+                </div>
+                <div className={styles.texts}>
+                    buildin mask object 의 경우 오브젝트를 클릭했을 때 refBuildingId 값을 반환합니다. 이 값은 연결된 building의 id 입니다.
+                </div>
+                <div className={styles.texts}> building id를 통해 building mask 로 지정된 object와 poi 를 show, hide 할 수 있습니다.</div>
+                <div className={styles.texts}> 하나의 building 에 대해서 building mask object는 여러개 일 수 있습니다.</div>
+                <div className={styles.texts}> 아래의 함수들을 통해 building mask object 를 관리할 수 있습니다.</div>
+                <pre>
+                    <code className={styles.code}>
+                        {` 
 // building 에 속한 모든 building mask 를 show 시킬 수 있습니다.
 map.context.showBuildingMaskByBuildingId(buildingId); 
 // building 에 속한 모든 building mask 를 hide 시킬 수 있습니다.
 map.context.hideBuildingMaskByBuildingId(buildingId); 
-            `}</code>
-        </pre>
+`}
+                    </code>
+                </pre>
+            </div>
+        </div>
     </div>
 );
 
