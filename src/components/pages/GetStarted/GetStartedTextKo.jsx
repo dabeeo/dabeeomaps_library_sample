a719e166d362a89d38be094b77fc6a577dbcbf0b
Clara(신상림) <sangrim.shin@dabeeo.com>
2025-06-16 18:06:25 +0900

    release note link 변경

--- src/components/pages/GetStarted/GetStartedTextKo.jsx
+++ src/components/pages/GetStarted/GetStartedTextKo.jsx
@@ -4,9 +4,7 @@ const textKo = (
     <div className={styles.getStartedText}>
         <div className={styles.title}>Getting Started</div>
         <div className={styles.miniTitle}>1. 다비오맵스 플랫폼 서버에서 지도인증정보를 획득합니다.</div>
-        <div className={styles.texts}>
-            * API에서 지도를 불러오기 위해서는 해당 지도의 Client ID / Secret code를 알아야 합니다. 아래의 절차에 따라 정보를 확인하세요.
-        </div>
+        <div className={styles.texts}>* API에서 지도를 불러오기 위해서는 해당 지도의 Client ID / Secret code를 알아야 합니다. 아래의 절차에 따라 정보를 확인하세요.</div>
         <p>
             1.
             <a href="https://dabeeomaps.com/" target="_blank">
@@ -24,8 +22,8 @@ const textKo = (
         <div className={styles.miniTitle}>1) 링크를 통하여 다비오맵스 API를 불러오는 방법.</div>
         <div className={styles.texts}>
             * 다비오맵스 JS API의 공식 api 링크는
-            <a href="https://dabeeomaps.com/" target="_blank">
-                [다비오맵스 실내지도 플랫폼]
+            <a href="https://dabeeo.notion.site/api4-library-release-note" target="_blank">
+                [Release Note]
             </a>
             에서 확인할 수 있습니다.
         </div>
@@ -42,11 +40,7 @@ const dabeeoMaps = new dabeeo.Maps();`}</code>
                     [https://www.npmjs.com/package/dabeeomaps]
                 </a>
                 입니다.
-                <br />* 다비오맵스 npm install을 통한 example은
-                <a href="https://github.com/dabeeo/dabeeomaps_library_boilerplate" target="_blank">
-                    [다비오맵스 실내지도 플랫폼 npm install boilerplate]
-                </a>
-                에서 확인할 수 있습니다.
+                <br />* 다비오맵스 npm install을 통한 example은 npm 페이지에서 확인할 수 있습니다.
             </p>
         </div>
         <pre>
